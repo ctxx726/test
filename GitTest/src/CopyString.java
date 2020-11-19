ccaec86104aae4870aa988126db1e2783e0693c4
tcxx726 <1094877764@qq.com>
2020-11-20 02:49:44 +0800

    test

---
+++ GitTest/src/CopyString.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class CopyString {
+
+	public static void main(String[] args) {
+		System.out.println(new Scanner(System.in).useDelimiter("end").next().replace("\n", "\\n").replace("\t", "\\t"));
+		/*
+		 * Scanner scanner = new Scanner(System.in); String t = ""; String s = "";
+		 * while(scanner.hasNextLine()) { t += scanner.nextLine(); t += "\\n"; }; s =
+		 * escape(s,t); System.out.println(s);
+		 */
+	}
+
+	/*
+	 * public static String escape(String s, String t) { t = t.replace("\t",
+	 * "\\\\t"); if (t.substring(t.length()-3,t.length()).equals("g\\n")) { s =
+	 * t.substring(0,t.length()-2); return s; } return t; }
+	 */
+
+}
